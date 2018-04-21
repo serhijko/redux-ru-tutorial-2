00603ba67705a1245a039766c5cfdbbd121d1003
serhij <sp_by@tut.by>
2018-04-21 21:24:46 +0300

    add Provider

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # redux-ru-tutorial-2
 Code for book:
 Redux RU tutorial 2
+Код для [Точка входа](http://sp.carkva-gazeta.by/redux-ru-tutorial-2/tochka_vhoda.html)
+Добавлен <Provider />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import { App } from './containers'
+import { Provider } from 'react-redux'
+import App from './containers'
+import configureStore from './store/configureStore'
 
+const store = configureStore()
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
